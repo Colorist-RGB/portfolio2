33dcc0809757bbc6a8b186d26009770b9720afc3
강병수 <77268939+Colorist-RGB@users.noreply.github.com>
2021-03-21 09:13:28 +0900

    ver 2.2

--- js/login.js
+++ js/login.js
@@ -1,7 +1,7 @@
 // 로그인 반응형 추가제어
 var ww = $(window).width()
 if (ww <= 450 && ww >= 302) {
-    $('.text2 p').eq(1).html('<p><span>다양한 혜택</span>이 준비되어있습니다.</p>')
+    $('.text2 p').eq(1).html('<p><span>다양한 혜택</span>이 준비되어있습니다.</p>').css({paddingBottom: '0'})
 }
 if (ww <= 301) {
     $('.text1').html('<p>비밀번호는 대소문자를 구분합니다.</p>')
